aeb9158b61aa46471a0a8dc06e8fdd6aa05225ec
gohan-1 <gohanthe@gmail.com>
2020-10-23 00:05:31 +0530

    finally adder requirements.txt1

--- api_basics/urls.py
+++ api_basics/urls.py
@@ -1,14 +1,15 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import train_model,scrap,predict,languages,translate
+# from .views import train_model,scrap,predict,languages,translate
+from .views import languages,translate
 
 
 urlpatterns = [
 
-    path('train/', train_model),
-    path('scrap/', scrap),
-    path('predict/', predict),
-    path('languages/',languages ),
+    # path('train/', train_model),
+    # path('scrap/', scrap),
+    # path('predict/', predict),
+    # path('languages/',languages ),
     path('translate/',translate)
 ]
